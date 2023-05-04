c1840607988b9d4677b236b4b442674c27464e71
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2023-05-03 23:04:41 -0700

    Switch to ts and change up routes

---
+++ calendar-component/src/auth/authHomepage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function AuthHomepage() {
+  return (
+    <div>
+      <h1>Auth</h1>
+    </div>
+  )
+}
---
+++ calendar-component/src/auth/loginHomepage.tsx
@@ -0,0 +1,7 @@
+export default function LoginHomepage() {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  )
+}
